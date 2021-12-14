103319728b436d212af8920da2b62fe31c7522ba
Bünyamin Kardeş <93006901+bunyaminkardes@users.noreply.github.com>
2021-12-14 17:17:30 +0300

    final

--- konuac.php
+++ konuac.php
@@ -1,20 +1,18 @@
 <?php
 	require_once("kutuphane.php");
 	$kullanici = girisyapankullanici();
-	
-	if(!isset($_SESSION['girisyapankullanici'])) // giriş yapmayan konu da açamasın.
+	if(!isset($_SESSION['girisyapankullanici'])) // giriş yapmayan kullanıcılar konu açma sayfasına giremesin.
 	{
 		header("Refresh: 2; url=index.php");
 		echo "Konu açabilmek için ilk önce giriş yapmanız lazım."."<br/>"."Anasayfaya yönlendiriliyorsunuz, lütfen bekleyin...";
 		exit();
 	}
-	else if($kullanici['bandurumu']==1) // yasaklı kullanıcılar da konu açamasın.
+	else if($kullanici['bandurumu']==1) // yasaklı kullanıcılar da konu açma sayfasına giremesin.
 	{
 		header("Refresh: 2; url=index.php");
 		echo "Yasaklı kullanıcı olduğunuz için konu açamazsınız."."<br/>"."Anasayfaya yönlendiriliyorsunuz, lütfen bekleyin...";
 		exit();
 	}
-
 	$content = 'contents/konuac-content.php';
 	require_once("masterpage.php");
 ?>
