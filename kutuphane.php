cb1abdbcdef81fdd99174e81fc916593748108ff
Bünyamin Kardeş <93006901+bunyaminkardes@users.noreply.github.com>
2022-04-20 21:08:26 +0300

    ufak düzenlemeler

--- kutuphane.php
+++ kutuphane.php
@@ -1,12 +1,13 @@
 <?php
 @session_start();
 
+define('CONNECTIONSTRING', 'mysql:host=localhost; dbname=dusozluk_bunyaminkardes; charset=utf8');
+define('USERNAME','root');
+define('PASSWORD','');
+
 try
 {
-	$connectionstring = 'mysql:host=localhost; dbname=dusozluk_bunyaminkardes; charset=utf8';
-	$kullaniciadi = 'root';
-	$sifre = "";
-	$baglanti = new PDO($connectionstring,$kullaniciadi,$sifre);
+	$baglanti = new PDO(CONNECTIONSTRING,USERNAME,PASSWORD);
 	$baglanti->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e)
@@ -14,9 +15,23 @@ catch(PDOException $e)
 	echo "VERİTABANI BAĞLANTI HATASI!!! : ". $e->getMessage();
 }
 
+function baglan()
+{
+	try
+	{
+		$baglanti = new PDO(CONNECTIONSTRING,USERNAME,PASSWORD);
+		$baglanti->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return $baglanti;
+	}
+	catch(PDOException $e)
+	{
+		echo "VERİTABANI BAĞLANTI HATASI!!! : ". $e->getMessage();
+	}
+}
+
 function girisyapankullanici()  // bu fonksiyon giriş yapan kullanıcıya ait kullanıcı bilgilerini bir dizi olarak return eder.
 {
-	$baglanti = new PDO("mysql:host=localhost; dbname=dusozluk_bunyaminkardes","root","");
+	$baglanti = baglan();
 	$kullaniciadi = @$_SESSION['girisyapankullanici'];
 	$sorgu = $baglanti->prepare("SELECT * FROM uyeler WHERE kullaniciadi = :kullaniciadi");
 	$sorgu->bindParam(":kullaniciadi",$kullaniciadi,PDO::PARAM_STR);
@@ -24,18 +39,20 @@ function girisyapankullanici()  // bu fonksiyon giriş yapan kullanıcıya ait k
 	$row = $sorgu->fetch();
 	return $row;
 }
+
 function kullanicilar() // bu fonksiyon veritabanındaki tüm kullanıcıları bir dizi olarak return eder.
 {
-	$baglanti = new PDO("mysql:host=localhost; dbname=dusozluk_bunyaminkardes","root","");
+	$baglanti = baglan();
 	$sorgu = $baglanti->prepare("SELECT * FROM uyeler");
 	$sorgu->execute();
 	$row = $sorgu->fetchAll();
 	return $row;
 }
+
 function seo_link($s) // bu fonksiyon parametre olarak gelen linki seo dostu link olarak geri return eder.
 {
 	$tr = array('ş','Ş','ı','I','İ','ğ','Ğ','ü','Ü','ö','Ö','Ç','ç','(',')','/',' ',',','?');
-	$eng = array('s','s','i','i','i','g','g','u','u','o','o','c','c','','','-','-','',''); 
+	$eng = array('s','s','i','i','i','g','g','u','u','o','o','c','c','','','-','-','','');
 	$s = str_replace($tr,$eng,$s);
 	$s = strtolower($s);
 	$s = preg_replace('/&amp;amp;amp;amp;amp;amp;amp;amp;amp;.+?;/', '', $s);
