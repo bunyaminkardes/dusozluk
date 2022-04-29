ce56715778377640f29432767d6bdace1184bc1b
Bünyamin Kardeş <93006901+bunyaminkardes@users.noreply.github.com>
2022-04-29 23:53:48 +0300

    Update sifreyisifirla-content.php

--- contents/sifreyisifirla-content.php
+++ contents/sifreyisifirla-content.php
@@ -1,8 +1,8 @@
 <?php
 require_once("phpmailer/class.phpmailer.php");
-define('MAILHOST','mail.dusozluk.com');
-define('MAILUSERNAME','iletisim.dusozluk.com');
-define('MAILPASSWORD','0102redpenciL');
+define('MAILHOST','');
+define('MAILUSERNAME','');
+define('MAILPASSWORD','');
 $hedefMail = $_POST['mailadresi'];
 ?>
 
